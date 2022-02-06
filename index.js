1deec2bcdda669f39451453c1d4af8c0f2f5b671
Brian Jomo <98019316+BrianJomo@users.noreply.github.com>
2022-02-06 20:21:18 +0300

    It will extend the function in the website.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function inbutton() {
   // code here
   //document.write("Time starts now!");
+  var score = 0;
+  var currentAnswer1 = document.one.quiz1.value;
+  var currentAnswer2 = document.one.quiz2.value;
+  var currentAnswer3 = document.one.quiz3.value;
+  var currentAnswer4 = document.one.quiz4.value;
+  var currentAnswer5 = document.one.quiz5.value;
+  var currentAnswer6 = document.one.quiz6.value;
+
